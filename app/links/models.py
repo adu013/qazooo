05a4ffd7202e1ba46339af9ae6f1a4061c56250b
Arindam Dutta <arindamdutta132@gmail.com>
2024-08-28 17:22:46 +0800

    Added model link

--- app/links/models.py
+++ app/links/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from common.db import models as common_models
+
+User = get_user_model()
+
+
+class Link(common_models.DateStampedModel):
+    created_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    original_url = models.URLField(max_length=1000, blank=False, null=False)
+    short_url = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        if len(self.original_url) > 25:
+            return f"{self.original_url[:21]} ..."
+        return self.original_url
