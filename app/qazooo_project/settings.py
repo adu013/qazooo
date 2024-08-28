6ffd8a5447b58051f1c79505cdd02585db407492
Arindam Dutta <arindamdutta132@gmail.com>
2024-08-29 01:59:21 +0800

    Added page not found page

--- app/qazooo_project/settings.py
+++ app/qazooo_project/settings.py
@@ -72,7 +72,9 @@ ROOT_URLCONF = "qazooo_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
