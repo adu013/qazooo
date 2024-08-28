b2950490ac0a69e82a4003fcefc3301ac8aa99ab
Arindam Dutta <arindamdutta132@gmail.com>
2024-08-28 13:59:27 +0800

    Added model class to common app

--- app/common/db/models.py
+++ app/common/db/models.py
@@ -1,3 +1,13 @@
+# common/models.py
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class DateStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
