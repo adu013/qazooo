9ed6e95e0a6a0155a34b3c6abf0d8a0b3b507f81
Arindam Dutta <arindamdutta132@gmail.com>
2024-08-30 03:54:25 +0800

    Updated settings: refacctor

--- app/qazooo_project/settings.py
+++ app/qazooo_project/settings.py
@@ -22,21 +22,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 if PROD:
-    pass
+    # SECURITY WARNING: keep the secret key used in production secret!
 
-else:
-    SECRET_KEY = "django-insecure-afse%yk&+b46twp^sv!z3*s&%x#q04qr1p)zqb+!m^d0w#vt$c"
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = False
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-if PROD:
     ALLOWED_HOSTS = [
         "https://qaz.ooo",
     ]
 else:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = "django-insecure-afse%yk&+b46twp^sv!z3*s&%x#q04qr1p)zqb+!m^d0w#vt$c"
+
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+
     ALLOWED_HOSTS = [
         "localhost",
     ]
