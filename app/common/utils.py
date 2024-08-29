a2862b9ce4f26ccba368888bbbc8ff4927551447
Arindam Dutta <arindamdutta132@gmail.com>
2024-08-30 03:53:59 +0800

    Added qr image creation features

--- app/common/utils.py
+++ app/common/utils.py
@@ -1,4 +1,5 @@
 import base64
+import qrcode
 
 
 def get_encoded_str(original_str, start_position=0, length=8):
@@ -10,3 +11,8 @@ def get_encoded_str(original_str, start_position=0, length=8):
     encoded_str = base64.urlsafe_b64encode(original_str.encode('utf-8'))
     truncated_str = encoded_str[start_position:end_position]
     return truncated_str.decode('utf-8')
+
+
+def create_qr_code(string):
+    img = qrcode.make(string)
+    return img
--- app/links/models.py
+++ app/links/models.py
@@ -1,4 +1,11 @@
+import qrcode
+
+from io import BytesIO
+from PIL import Image
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.core.files import File
 from django.db import models
 
 from common.db import models as common_models
@@ -12,7 +19,9 @@ class Link(common_models.DateStampedModel):
     original_url = models.URLField(max_length=1000, blank=False, null=False)
     short_url = models.CharField(max_length=50)
 
-    qr_img = models.ImageField(default=None, blank=True, null=True)
+    qr_img = models.ImageField(
+        default=None, blank=True, null=True, upload_to="qrs"
+    )
 
     link_audit = models.OneToOneField(
         "analytics.LinkAudit",
@@ -27,3 +36,16 @@ class Link(common_models.DateStampedModel):
         if len(self.original_url) > 25:
             return f"{self.original_url[:21]} ..."
         return self.original_url
+
+    def save(self, *args, **kwargs):
+        host = settings.MAIN_HOST
+        full_url = host + self.short_url
+        qr_image = qrcode.make(full_url)
+        qr_offset = Image.new('RGB', (310, 310), 'white')
+        qr_offset.paste(qr_image)
+        file_name = f"{self.short_url}-qr.png"
+        stream = BytesIO()
+        qr_offset.save(stream, 'PNG')
+        self.qr_img.save(file_name, File(stream), save=False)
+        qr_offset.close()
+        return super().save(*args, **kwargs)
