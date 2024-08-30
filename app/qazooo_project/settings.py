c8a8b399f875b94c8cd6037ee47b147e68159218
Arindam D <arindamdutta132@gmail.com>
2024-08-30 17:39:41 +0800

    Update settings.py

--- app/qazooo_project/settings.py
+++ app/qazooo_project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
-PROD = os.environ.get('LOCAL_DJANGO_PROJECT_QAZOOO', False)
+PROD = os.environ.get('IS_DJANGO_PROJECT_QAZOOO_PROD_ENV', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
